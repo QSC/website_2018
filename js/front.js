7dc4bf8584abaa0f8762a72a79094fa00daca149
Cormac Crowley <Cory@Cormacs-MBP.lan>
2017-05-11 18:27:13 -0700

    Updated landing page: scroll down to access website, navbar follows window, main body div structure

--- js/front.js
+++ js/front.js
@@ -75,7 +75,7 @@ function utils() {
 
     /* tooltips */
 
-    $('[data-toggle="tooltip"]').tooltip();
+    /*$('[data-toggle="tooltip"]').tooltip();/*
 
     /* click on the box activates the radio */
 
