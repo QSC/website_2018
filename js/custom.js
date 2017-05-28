85fb376a55883b529cde9583f3d7986b64d51d5e
= <ccrowley96@gmail.com>
2017-05-28 13:38:43 -0700

    removed flare on small screens due to fade problem on mobile

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,12 @@
 $('.navbar-collapse ul li a').click(function(){ 
         $('.navbar-toggle:visible').click();
 });
+
+//Remove color flare with jQuery on small screens
+if ($(window).width() <= 800){	
+		// do something here
+		$("#target_flare").remove();
+		$("#target_flarer").remove();
+		$("#target_flareg").remove();
+}	
+
